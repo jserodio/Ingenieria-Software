3cdff5f3b00b7ad120e18b160cfb84e9abfb42a9
asalbidegoitia <asisalbide@gmail.com>
2016-01-26 00:34:18 +0100

    prueba asier

--- Ingenieria-Software/src/prueba/test.java
+++ Ingenieria-Software/src/prueba/test.java
@@ -4,8 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 
-		System.out.println("jserodio");
-		// TE HE CREADO YO LA RAMA DESDE MI ECLIPSE LMAO
+		System.out.println("asier");
+		
 	}
 
 }
