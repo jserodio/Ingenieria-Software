4ea92ce57d25d4607f7fe67e1d6f67b08b3dd7e9
Asier Oleaga <asieroleaga@gmail.com>
2016-05-12 18:57:24 +0200

    Clase partida

---
+++ Ingenieria-Software/packModelo/Partida.java
@@ -0,0 +1,12 @@
+package packModelo;
+
+public class Partida {
+	private String nombre;
+	private int puntuacion;
+	
+	public Partida(String pNombre, int pPuntuacion){
+		nombre=pNombre;
+		puntuacion=pPuntuacion;
+	}
+
+}
