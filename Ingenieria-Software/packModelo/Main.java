e97ab95146cdc74b9bd1d2611675359966ccf15c
jserodio <serodio.jose@protonmail.ch>
2016-05-13 11:57:08 +0200

    muchos cambios

--- Ingenieria-Software/packModelo/Main.java
+++ Ingenieria-Software/packModelo/Main.java
@@ -1,14 +1,12 @@
 package packModelo;
 
-import packInterfaz.VElegirNivel;
 import packInterfaz.VLogin;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Conexion conexion = new Conexion();
-		conexion.conectar();
-		new VLogin();
+		Sesion.getSesion().iniciarCrono();
+		new VLogin();		
 	}
 
 }
